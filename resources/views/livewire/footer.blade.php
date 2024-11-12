fe8194bad95d5e0999162a017e8d4960c9f1d6c0
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-11-12 15:22:45 +0800

    foter

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,14 +1,19 @@
-<div class="">
+<div class="container mx-auto">
 
 
     <div class="flex  items-center justify-between">
 
         <p>
-             {{'@'.now()->year}} STEPP APP TBI
+             &#169 {{now()->year}} STEPP APP TBI
         </p>
 
-        <div>
+        <div   class="flex items-center justify-center">
+            <p>
+                Partnership project by
 
+            </p>
+            <img src="{{asset('images/placeholder-image.jpg')}}" alt="" class="w-24 h-24">
+            <img src="{{asset('images/placeholder-image.jpg')}}" alt="" class="w-24 h-24">
         </div>
     </div>
     {{-- The best athlete wants his opponent at his best. --}}
